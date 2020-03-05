519e2e9844cd9d2353825831a08d59907e4b57ee
Rafael Ribeiro <rafael.s.ribeiro@sc.senai.br>
2020-03-04 21:25:32 -0300

    Criação de Layout

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package app;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -11,7 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("PDI - Processador Digital de Imagens");
+        primaryStage.setMaximized(true);
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
